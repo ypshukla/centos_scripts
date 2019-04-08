6e2e49405b01d889690b7e097c8a206881fd7243
ypshukla <yogeshwar.shukla@vidyamantra.com>
2019-04-08 10:25:53 +0530

    script updated

--- remove_google-chrome.sh
+++ remove_google-chrome.sh
@@ -6,16 +6,13 @@
 yum remove -y -q google-chrome*
 
 #yum remove -y -q google-chrome-stable
-# To remove version specific
+## To remove version specific
 #yum remove -y -q google-chrome-beta
 #yum remove -y -q google-chrome-unstable
 
-# Find the current user (created during installation)
-export CUSER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
-
 # Delete chrome from everywhere
-rm -rf /home/$CUSER/.cache/google-chrome*
-rm -rf /home/$CUSER/.config/google-chrome*
+rm -rf $HOME/.cache/google-chrome*
+rm -rf $HOME/.config/google-chrome*
 
 # Clear the yum caches
 yum clean all >> /dev/null
